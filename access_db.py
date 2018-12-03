f057706fa3e638a17b360af61e6d8286a8a07168
cape <carlos.perales95@opendeusto.es>
2018-12-03 19:10:39 +0100

    Vie, insert, update and delete methods for db

--- access_db.py
+++ access_db.py
@@ -3,7 +3,7 @@
 import MySQLdb
 
 # Open database connection
-db = MySQLdb.connect("localhost","testuser","test123","lab_resourcer" )
+db = MySQLdb.connect("localhost","root","CAPE95skype-con","lab_resourcer" )
 
 # prepare a cursor object using cursor() method
 cursor = db.cursor()
@@ -14,6 +14,69 @@ cursor.execute("SELECT VERSION()")
 # Fetch a single row using fetchone() method.
 data = cursor.fetchone()
 print "Database version : %s " % data
+
+#######READ FROM TABLE############
+sql = "SELECT * FROM chemical" \
+       #WHERE INCOME > '%d'" % (1000)
+try:
+   # Execute the SQL command
+   cursor.execute(sql)
+   # Fetch all the rows in a list of lists.
+   results = cursor.fetchall()
+   for row in results:
+      chemid = row[0]
+      chemname = row[1]
+      # Now print fetched result
+      print "chemid=%s,chemname=%s" % \
+             (chemid, chemname)
+except:
+   print "Error: unable to fecth data"
+#####################################
+
+#######INSERT INSTANCE TO TABLE############
+# Prepare SQL query to INSERT a record into the database.
+sql = """INSERT INTO chemical(chemical_id,
+         name)
+         VALUES ('20', 'brule')"""
+try:
+   # Execute the SQL command
+   cursor.execute(sql)
+   # Commit your changes in the database
+   db.commit()
+except:
+   # Rollback in case there is any error
+   db.rollback()
+ #####################################
+
+
+#######UPDATE TABLE############
+# Prepare SQL query to UPDATE required records
+sql = "UPDATE chemical SET name = 'test' WHERE chemical_id = 20"
+try:
+   # Execute the SQL command
+   cursor.execute(sql)
+   # Commit your changes in the database
+   db.commit()
+except:
+   # Rollback in case there is any error
+   db.rollback()
+#####################################
+
+
+#######DELETE INSTANCE FROM TABLE############
+# Prepare SQL query to DELETE required records
+sql = "DELETE FROM chemical WHERE name = 'test'" #% (20)
+try:
+   # Execute the SQL command
+   cursor.execute(sql)
+   # Commit your changes in the database
+   db.commit()
+except:
+   # Rollback in case there is any error
+   db.rollback()
+#####################################
+
+
 
 # disconnect from server
 db.close()
