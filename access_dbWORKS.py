68313e6585921f82bd7d65e65cacb0b61e351a17
cape <carlos.perales95@opendeusto.es>
2018-12-14 18:05:25 +0100

    MAin program properly separated and annotated

--- access_dbWORKS.py
+++ access_dbWORKS.py
@@ -2,9 +2,15 @@
 import credentials
 import MySQLdb
 
+
+# --------------------- DB FUNCTIONS ---------------------- #
+
+
 queries = { "primary": "SELECT * FROM chemical WHERE chemical_id = ANY ( SELECT primary_antibody_id FROM primary_antibody )",
             "staining": "SELECT * FROM chemical WHERE chemical_id = ANY ( SELECT staining_id FROM staining )"
             }
+
+
 
 def query(option):
     sql = queries[option]
@@ -20,7 +26,6 @@ def query(option):
           print "%s.- %s" % (chemid, chemname)
     except:
        print "Error: unable to fetch data"
-
 
 
 def get_chemical_name(id):
@@ -78,6 +83,7 @@ def get_pa_animal(pa_id):
 
     return result[0]
 
+
 def get_sec_antibody(pa_id,s_id):
     sql = "SELECT secondary_antibody_id FROM binding WHERE primary_antibody_id = %s and staining_id = %s " % (pa_id,s_id)
 
@@ -106,13 +112,91 @@ def show_researchers():
        print "Error: unable to fetch researchers"
 
 
+def check_availability(chemical_id):
+
+    sql = "SELECT * FROM inventory_chemical WHERE chemical_id = %s" % (chemical_id)
+    amount = 0
+    try:
+        # Execute the SQL command
+        cursor.execute(sql)
+        # Fetch all the rows in a list of lists.
+        results = cursor.fetchall()
+        for row in results:
+            amount = row[2]
+
+    except:
+        print "Error: unable to fetch data"
+
+
+    res_amount = check_reserved(chemical_id)
+
+    for reserve in res_amount:
+        amount -= reserve
+        print "- %.3f" %(reserve)
+
+    print "Available quant: %.3f" %(amount)
+    return amount
+
+
+def check_reserved(chemical_id):
+
+    sql = "SELECT * FROM experiment_chemical WHERE chemical_id = %s" % (chemical_id)
+    amount = []
+    try:
+        # Execute the SQL command
+        cursor.execute(sql)
+        # Fetch all the rows in a list of lists.
+        results = cursor.fetchall()
+        for row in results:
+            amount.append(row[2])
+            print(amount)
+
+    except:
+        print "Error: unable to fetch data"
+
+    return amount
+
+
+def create_experiment(researcher_id,n_slices):
+    sql = "INSERT INTO experiment (researcher_id,tissue_slices) VALUES (%s,%s)" % (researcher_id,n_slices)
+
+    try:
+        cursor.execute(sql)
+        db.commit()
+        return cursor.lastrowid
+    except:
+        db.rollback()
+        return -1
+
+
+def reserve_chemicals(experiment_id,chemicals):
+
+    for chemical in chemicals:
+
+        sql = "INSERT INTO experiment_chemical(experiment_id,chemical_id,amount)VALUES (%s,%s,%s)" % (experiment_id,chemical[0],chemical[1])
+
+        try:
+           # Execute the SQL command
+           cursor.execute(sql)
+           # Commit your changes in the database
+           db.commit()
+
+        except:
+           # Rollback in case there is any error
+           db.rollback()
+
+
+# --------------------------------------------------------- #
+
+
+
+# -------------- CHEMICAL FORMULA FUNCTIONS --------------- #
 
 
 def get_chem_step(step, n_slices):
     steps={"degrease":2,"antigen":3,"wash":5,"coverslip":8}
     quant = (n_slices//8)+1
     quant = 60 * quant
-    print (quant)
     return steps[step], quant
 
 
@@ -148,7 +232,7 @@ def get_chem_prim_antibody(n_slices, pa_id):
     pa_dilution = get_pa_dilution(pa_id)
 
     final_dilution = pa_dilution/2 #Get pa_diution from query
-    pa_amount = total * final_dilution
+    pa_amount = total / final_dilution
     chems.append((pa_id,pa_amount))
 
     return chems
@@ -163,6 +247,95 @@ def get_chem_sec_antibody(n_slices, pa_id, s_id):
 
 def get_chem_staining(n_slices, s_id):
     return s_id, (0.2 * n_slices)
+
+
+# --------------------------------------------------------- #
+
+
+
+# --- CHEMICAL via PROTOCOL STEPS CALCULATION FUNCTIONS --- #
+
+
+def header_block(chemicals, n_slices):
+
+    #chemicals.append(get_chem_step("degrease", n_slices)) #degrease
+    chemicals.append(get_chem_step("antigen", n_slices))#antigen
+    chemicals.append(get_chem_step("wash", n_slices))#wash
+
+    chems = get_chem_block1(n_slices)#block1
+    dissolve_inTuple(chemicals, chems)
+
+    chemicals.append(get_chem_step("wash", n_slices))#wash
+
+    chems = get_chem_block2(n_slices)#block2
+    dissolve_inTuple(chemicals, chems)
+
+
+def step_PaSaS(chemicals, n_slices, pa_id, s_id):
+
+    chems = get_chem_prim_antibody(n_slices, pa_id)#pa
+    dissolve_inTuple(chemicals, chems)
+
+    #single wash step located outside in calculate_variation_steps()
+
+    chemicals.append(get_chem_sec_antibody(n_slices, pa_id, s_id))#sa
+    chemicals.append(get_chem_step("wash", n_slices))#wash
+
+    chemicals.append(get_chem_staining(n_slices, s_id))#staining
+    chemicals.append(get_chem_step("wash", n_slices))#wash
+
+
+def repeating_block(chemicals, n_slices, pa_id , s_id):
+    header_block(chemicals, n_slices)
+    step_PaSaS(chemicals, n_slices, pa_id , s_id)
+    chemicals.append(get_chem_step("wash", n_slices))#wash
+
+
+def calculate_variation_steps(n_slices, primaries, stainings):
+
+    chemicals = []
+    equal_pas = check_compatibility(primaries)
+    #print(equal_pas)
+    chemicals.append(get_chem_step("degrease", n_slices)) #degrease
+
+    if len(equal_pas) == 1:
+        #print "Todos diferentes: "
+        for index, primary in enumerate(primaries):
+            #print "Pasada - "
+            repeating_block(chemicals, n_slices, primary, stainings[index])
+        #print "\n"
+    else:
+        #print "Algunos estan iguals: "
+        remaining_p = primaries[:]
+        remaining_s = stainings[:]
+        header_block(chemicals, n_slices)
+        for item in equal_pas:
+            step_PaSaS(chemicals, n_slices, primaries[item], stainings[item])
+            #print(primaries)
+            #print(stainings)
+            num = item
+            del remaining_p[0]
+            del remaining_s[0]
+            #print "Pasada -"
+
+        chemicals.append(get_chem_step("wash", n_slices))#wash
+        #print "\n"
+
+        if len(remaining_p) == 1:
+            #print "Parece que eran todos: "
+            repeating_block(chemicals, n_slices, remaining_p.pop(0), remaining_s.pop(0))
+            #print "Lonely pasada"
+            #print "\n"
+
+    chemicals.append(get_chem_step("coverslip", n_slices))#coverslip
+    return chemicals
+
+
+# --------------------------------------------------------- #
+
+
+
+# ------------------------- UTILS ------------------------- #
 
 
 def dissolve_inTuple(dest_array, orig_array):
@@ -206,84 +379,18 @@ def indexall(list, value):
     return [i for i, v in enumerate(list) if v == value]
 
 
-def header_block(chemicals, n_slices):
-
-    chemicals.append(get_chem_step("antigen", n_slices))#antigen
-    chemicals.append(get_chem_step("wash", n_slices))#wash
-
-    chems = get_chem_block1(n_slices)#block1
-    dissolve_inTuple(chemicals, chems)
-
-    chemicals.append(get_chem_step("wash", n_slices))#wash
-
-    chems = get_chem_block2(n_slices)#block2
-    dissolve_inTuple(chemicals, chems)
+# --------------------------------------------------------- #
 
 
-def step_PaSaS(chemicals, n_slices, pa_id, s_id):
 
-    chems = get_chem_prim_antibody(n_slices, pa_id)#pa
-    dissolve_inTuple(chemicals, chems)
-
-    #single wash step located outside in calculate_variation_steps()
-
-    chemicals.append(get_chem_sec_antibody(n_slices, pa_id, s_id))#sa
-    chemicals.append(get_chem_step("wash", n_slices))#wash
-
-    chemicals.append(get_chem_staining(n_slices, s_id))#staining
-    chemicals.append(get_chem_step("wash", n_slices))#wash
-
-
-def repeating_block(chemicals, n_slices, pa_id , s_id):
-    header_block(chemicals, n_slices)
-    step_PaSaS(chemicals, n_slices, pa_id , s_id)
-    chemicals.append(get_chem_step("wash", n_slices))#wash
-
-
-def calculate_variation_steps(n_slices, primaries, stainings):
-
-    chemicals = []
-    equal_pas = check_compatibility(primaries)
-    print(equal_pas)
-    chemicals.append(get_chem_step("degrease", n_slices)) #degrease
-
-    if len(equal_pas) == 1:
-        print "Todos diferentes: "
-        for index, primary in enumerate(primaries):
-            print "Pasada - "
-            repeating_block(chemicals, n_slices, primary, stainings[index])
-        print "\n"
-    else:
-        print "Algunos estan iguals: "
-        remaining_p = primaries[:]
-        remaining_s = stainings[:]
-        header_block(chemicals, n_slices)
-        for item in equal_pas:
-            step_PaSaS(chemicals, n_slices, primaries[item], stainings[item])
-            print(primaries)
-            print(stainings)
-            num = item
-            del remaining_p[0]
-            del remaining_s[0]
-            print "Pasada -"
-
-        chemicals.append(get_chem_step("wash", n_slices))#wash
-        print "\n"
-
-        if len(remaining_p) == 1:
-            print "Parece que eran todos: "
-            repeating_block(chemicals, n_slices, remaining_p.pop(0), remaining_s.pop(0))
-            print "Lonely pasada"
-            print "\n"
-
-    chemicals.append(get_chem_step("coverslip", n_slices))#coverslip
-    return chemicals
+# ------------ MAIN TERMINAL BASED FUNCTIONS -------------- #
 
 
 def input_PaSaloop(times):
 
     primaries = []
     stainings = []
+
     for i in range(0, times):
 
         print("     EXPERIMENT PREPARATION - INPUT PHASE: PRIMARY ANTIBODY SELECTION %i \n" %(i))
@@ -304,6 +411,24 @@ def input_PaSaloop(times):
         print("\n")
 
     return primaries, stainings
+
+
+def input_reservation():
+
+    print("     CHEMICAL RESERVATION - INPUT PHASE \n")
+    print("______________________________________________________________________\n")
+    choice = int(raw_input("Please enter the desired option: "))
+    #for availability give options
+    print("1.- Reserve available chemicals and request all unavailables")
+    print("2.- Reserve availables and manage requests")
+    print("3.- Reserve available chemicals only")
+    print("4.- Cancel")
+
+    if(choice < 1 or choice > 4):
+        print("That is not an option")
+        choice = input_reservation()
+    else:
+        return choice
 
 
 def input_phase():
@@ -350,14 +475,43 @@ def input_phase():
     return researcher_id,n_slices,primaries,stainings
 
 
+def input_reservation():
+    print("\n\n     CHEMICAL RESERVATION - INPUT PHASE \n")
+    print("______________________________________________________________________\n")
+    #for availability give options
+    print("1.- Reserve available chemicals and request all unavailables")
+    print("2.- Reserve availables and manage requests")
+    print("3.- Reserve available chemicals only")
+    print("4.- Cancel")
+
+    choice = int(raw_input("\nPlease enter the desired option: "))
+
+    if(choice < 1 or choice > 4):
+        print("%i is not an option.",choice)
+        choice = input_reservation()
+    else:
+        return choice
+
+
 def calculation_phase(n_slices, primaries, stainings):
 
     chemicals = calculate_variation_steps(n_slices, primaries, stainings)
-    print(chemicals)
-    print "\n"
     chemicals = join_equals(chemicals)
 
     return chemicals
+
+
+def split_chemicals_availability(chemicals):
+    available = []
+    unavailable = []
+
+    for chemical in chemicals:
+        if chemical[2]>=0:
+            available.append((chemical[0],chemical[1])) #Append chemcal_id and amount Needed
+        else:
+            unavailable.append((chemical[0],chemical[2]*(-1))) #Append chemical_id and necessary amount to request
+
+    return available, unavailable
 
 
 def output_phase(chemicals, researcher_id, n_slices, primaries, stainings):
@@ -366,7 +520,7 @@ def output_phase(chemicals, researcher_id, n_slices, primaries, stainings):
     researcher_name = get_researcher_name(researcher_id)
 
     print("     EXPERIMENT PREPARATION - OUTPUT PHASE\n")
-    print("______________________________________________________________________\n")
+    print("________________________\n")
     print "Researcher: %s - ID(%i) \n" %(researcher_name, researcher_id)
     print("Experiment ID: \n")
     print "Number of slices: %i \n" %(n_slices)
@@ -381,6 +535,17 @@ def output_phase(chemicals, researcher_id, n_slices, primaries, stainings):
     print("List of Chemicals Needed:")
     print("=========================")
     #list with availability
+
+#    for id, amount in chemicals:
+#        name = get_chemical_name(id)
+#        print "(%i)%s - %.3f ml" % (id, name, amount)
+
+
+    chemicals = reservation_phase(chemicals)
+
+    available, unavailable = split_chemicals_availability(chemicals)
+
+
     #for availability give options
     ##1. Reserve available chemicals and request all unavailables
     ##2. Reserve availables and manage requests
@@ -388,10 +553,47 @@ def output_phase(chemicals, researcher_id, n_slices, primaries, stainings):
     ##4. Cancel
     print("\n")
 
-    for id, quantity in chemicals:
-        #get_chemical name query = name
+    for id, amount, am_available  in chemicals:
+
         name = get_chemical_name(id)
-        print "(%i)%s - %.3f ml" % (id, name, quantity)
+
+        if am_available >= 0:
+            print "(%i) %s %.3f ml - AVAILABLE" % (id, name, amount)
+
+        else:
+            print "(%i) %s %.3f ml - UNAVAILABLE " %(id, name, amount)
+
+    choice = input_reservation()
+
+    if choice == 4:
+        print "Experiment cancelled"
+    else:
+        experiment_id = create_experiment(researcher_id,n_slices)
+        reserve_chemicals(experiment_id,available)
+        if choice == 1:
+            print("Chemicals reserved. All unavaiables should be requested")
+            #Request unavailable
+        elif choice == 2:
+            print("Chemicals reserved. Manage unavailable chemicals")
+            #Manage request unavailables
+
+
+
+def reservation_phase(chemicals):
+
+    final_chemicals = []
+
+    for id, quantity in chemicals:
+        #print(id)
+        #print(quantity)
+        amount = check_availability(id)
+        #print(amount)
+        final_chemicals.append((id, quantity, (amount-quantity)))
+        #print "Am-q: %.3f" % (amount-quantity)
+
+    #print(final_chemicals)
+
+    return final_chemicals
 
 
 def execution():
@@ -405,6 +607,9 @@ def execution():
     chemicals = calculation_phase(n_slices, primaries, stainings)
 
     output_phase(chemicals, researcher_id, n_slices, primaries, stainings)
+
+
+# --------------------------------------------------------- #
 
 
 
@@ -421,8 +626,6 @@ execution()
 
 #def inventory_checkup():
     #query to check the amounts available for chemicals[] in inventory and add status to dest_array
-
-
 
 
 db.close()
